a7c1ae08495b9f1f9fe73fa3342f9a7d06506b51
Tac Tacelosky <tacman@gmail.com>
2022-09-12 16:34:33 -0500

    run simple tests

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Acceptance;
+
+use App\Tests\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
