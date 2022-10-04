165c5ac6414241187317cb9175b328d184dd489f
Tac Tacelosky <tacman@gmail.com>
2022-10-04 15:43:46 -0400

    add twig, but still cant get it from LoggerSubscriber

--- src/EventSubscriber/LoggerSubscriber.php
+++ src/EventSubscriber/LoggerSubscriber.php
@@ -6,7 +6,10 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Codeception\Event\SuiteEvent;
 use Codeception\Events;
 use Codeception\Extension;
-use Codeception\Module\Symfony;  // smell test, something's wrong...
+use Codeception\Module\Symfony;
+use Twig\Environment;
+
+// smell test, something's wrong...
 
 
 class LoggerSubscriber  extends Extension implements EventSubscriberInterface //
@@ -22,17 +25,27 @@ class LoggerSubscriber  extends Extension implements EventSubscriberInterface //
     {
         /** @var Symfony $symfony */
         $symfony = $this->getModule('Symfony');
+        /** @var Environment $twig */
         $twig = $symfony->grabService('twig');
         assert($twig, "Twig is not loaded");
+        $results = $twig->render('{{ x }}', ['x' => 'yes']);
+        dd("yes, it works! " . $results);
     }
+
+//    public static function getSubscribedEvents(): array
+//    {
+//        return [
+//            Events::SUITE_AFTER => 'afterSuite',
+//        ];
+//    }
 
     public static function getSubscribedEvents(): array
     {
         return [
+            Events::SUITE_INIT => 'receiveModuleContainer',
             Events::SUITE_AFTER => 'afterSuite',
         ];
     }
-
 
 
 }
